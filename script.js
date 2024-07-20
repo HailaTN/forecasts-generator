2acbce80df15ca7fa5a61cd0ce4f7f3ea4e97f6a
HalinaTN <tonaganyan.galina@icloud.com>
2024-07-20 21:38:44 +0300

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,59 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+function generateNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+const forecastBtn = document.querySelector('.forecast-btn');
+
+
+forecastBtn.addEventListener('click', function () {
+    const name = document.querySelector('h1');
+    name.textContent = 'Тебя ждёт прекрасная неделя';
+    let probability = document.querySelector('.current-forecast p');
+    let predictionNumber = generateNumber(0, 100);
+    probability.textContent = `Вероятность : ${predictionNumber} %`;
+
+
+    const myPredictionContainer = document.querySelector('.forecasts');
+    const template = document.querySelector('#forecast-item');
+
+    if (predictionNumber > 50) {
+        const items = template.content.cloneNode(true);
+
+        items.querySelector('h3').textContent = 'Приглашение на свидание';
+        items.querySelector('p').textContent = `Вероятность : ${generateNumber(0, 100)} %`;
+
+        myPredictionContainer.append(items);
+
+        const items2 = template.content.cloneNode(true);
+
+        items2.querySelector('h3').textContent = 'Отдых от всех дел';
+        items2.querySelector('p').textContent = `Вероятность : ${generateNumber(0, 100)} %`;
+
+        myPredictionContainer.append(items2);
+
+        const items3 = template.content.cloneNode(true);
+
+        items3.querySelector('h3').textContent = 'Везде сопутствует удача';
+        items3.querySelector('p').textContent = `Вероятность : ${generateNumber(0, 100)} %`;
+
+        myPredictionContainer.append(items3);
+
+        const items4 = template.content.cloneNode(true);
+
+        items4.querySelector('h3').textContent = 'Получишь неожиданный подарок';
+        items4.querySelector('p').textContent = `Вероятность : ${generateNumber(0, 100)} %`;
+
+        myPredictionContainer.append(items4);
+    } else {
+        const items5 = template.content.cloneNode(true);
+        items5.querySelector('h3').textContent = 'Тебя ждёт неудача';
+        items5.querySelector('p').textContent = `Вероятность : ${generateNumber(0, 100)} %`;
+
+        myPredictionContainer.append(items5);
+    }
+})
